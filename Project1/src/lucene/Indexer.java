14e103a8dc9ff2a3a9d97d0275ea79e20e5ad6bf
William Lee <wksw9680@hotmail.com>
2013-09-11 00:34:05 +0800

    Added fields to indexer.java

--- Project1/src/lucene/Indexer.java
+++ Project1/src/lucene/Indexer.java
@@ -18,9 +18,13 @@ public class Indexer {
 		try {
 			for (Paper paper : papers) {
 				Document document = new Document();
-				// document.add(new Field("id", paper.getId(), Field.Store.YES, Field.Index.NO));
-				document.add(new Field("title", paper.getContent(), Field.Store.YES, Field.Index.ANALYZED));
+				document.add(new Field("id", paper.getId(), Field.Store.YES, Field.Index.NO));
+				document.add(new Field("title", paper.getTitle(), Field.Store.YES, Field.Index.ANALYZED));
 				// TODO: Add the rest of fields
+				document.add(new Field("summary", paper.getSummary(), Field.Store.NO, Field.Index.ANALYZED));
+				document.add(new Field("date", paper.getDate(), Field.Store.NO, Field.Index.NO));
+				document.add(new Field("author", paper.getAuthor(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+				document.add(new Field("keyword", paper.getKeyword(), Field.Store.NO, Field.Index.ANALYZED));
 				
 				indexWriter.addDocument(document);
 				indexCount++;
