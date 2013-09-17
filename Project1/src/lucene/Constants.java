af9dce41769f6eb4b6fff79a571d368acd28dff2
David Heryanto <david.heryanto@hotmail.com>
2013-09-17 20:58:51 +0800

    Refactor Window.java

--- Project1/src/lucene/Constants.java
+++ Project1/src/lucene/Constants.java
@@ -7,8 +7,11 @@ public class Constants {
 	public static final String DIR_PATH_DATA = "./data";
 	public static final String DIR_PATH_INDEX = "./index";
 	
-	
+	public static final String LABEL_FRAME = "Lucene Search Engine";
+	public static final String LABEL_TEXTFIELD = "Type Query";
 	public static final String LABEL_BUTTON_SEARCH = "Search";
 	public static final String LABEL_BUTTON_SEARCH_REFINE = "Refine Query";
+	public static final String LABEL_BUTTON_QUERY_IMPORT = "Import Query";
+	public static final String LABEL_BUTTON_QUERY_EXPORT = "Export Query";
 	
 }
--- Project1/src/lucene/Window.java
+++ Project1/src/lucene/Window.java
@@ -1,14 +1,14 @@
 package lucene;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 import java.util.Enumeration;
 
 import javax.swing.DefaultComboBoxModel;
@@ -29,6 +29,18 @@ import javax.swing.plaf.FontUIResource;
 public class Window {
 	// Singleton pattern
 	private static final Window instance = new Window();
+	
+	private static JFrame frame;
+	private static JTextField textField;
+	private static JScrollPane scrollPane;
+	private static JList<String> list;
+	
+	private static JButton importButton;
+	private static JButton exportButton;
+	
+	private static JComboBox<String> pseudoRFComboBox;
+	private static JComboBox<String> similarityComboBox;
+	private static JComboBox<String> searchTypeComboBox;
 
 	private Window() { }
 
@@ -36,26 +48,154 @@ public class Window {
 		return instance;
 	}
 	
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	public static void initialize(ListModel<String> model) {
+		setUI();
+		initFrame();
+		initSearchPanel();
+		initScrollPane(model);
+		initFilePanel();
+		
+		frame.setVisible(true);
+	}
 
+	private static void initScrollPane(ListModel<String> model) {
+		list = new JList<String>(model);
+		scrollPane = new JScrollPane(list);
+		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+	}
 
+	private static void initFilePanel() {
+		JPanel filePanel = new JPanel();
+		frame.getContentPane().add(filePanel, BorderLayout.SOUTH);
+		
+		// Add button to import file
+		GridBagLayout gbl_filePanel = new GridBagLayout();
+		gbl_filePanel.columnWidths = new int[]{250, 250, 0};
+		gbl_filePanel.rowHeights = new int[] {30};
+		gbl_filePanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_filePanel.rowWeights = new double[]{0.0};
+		filePanel.setLayout(gbl_filePanel);
+		
+		importButton = new JButton("Import Query");
+		importButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(5, 5, 5, 5);
+		gbc_btnNewButton_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnNewButton_1.gridx = 0;
+		gbc_btnNewButton_1.gridy = 0;
+		filePanel.add(importButton, gbc_btnNewButton_1);
+		
+		// Add button to export file
+		exportButton = new JButton("Export Result");
+		exportButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+		gbc_btnNewButton_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnNewButton_2.insets = new Insets(5, 5, 5, 5);
+		gbc_btnNewButton_2.gridx = 1;
+		gbc_btnNewButton_2.gridy = 0;
+		filePanel.add(exportButton, gbc_btnNewButton_2);
+	}
 
+	private static void initFrame() {
+		frame = new JFrame();
+		frame.setTitle("Lucene Search Engine");
+		frame.setSize(800, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// get screen properties
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+	    int x = (int) ((dim.getWidth() - frame.getWidth()) / 2);
+	    int y = (int) ((dim.getHeight() - frame.getHeight()) / 2);
+		frame.setLocation(x, y);
+	}
 
+	private static void initSearchPanel() {
+		JPanel searchPanel = new JPanel();
+		frame.getContentPane().add(searchPanel, BorderLayout.NORTH);
+		
+		// Setup GridBag Layout
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{127, 70, 50, 60, 100, 50, 0};
+		gbl_panel.rowHeights = new int[] {30};
+		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0};
+		searchPanel.setLayout(gbl_panel);
 
-	
+		// Add input field
+		textField = new JTextField("Type query");
+		textField.addActionListener(Controller.getInstance());
 
-	private static JFrame frmLuceneSearchEngine;
-	private static JTextField textField;
-	private static JScrollPane scrollPane;
-	private static JList list;
-	private static JButton btnNewButton_1;
-	private static JButton btnNewButton_2;
-	private static JComboBox comboBox;
-	private static JComboBox comboBox_1;
-	private static JLabel lblNewLabel;
-	private static JLabel lblNewLabel_1;
-	private static JComboBox comboBox_2;
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(5, 5, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 0;
+		gbc_textField.gridy = 0;
+		searchPanel.add(textField, gbc_textField);
+		
+		// Add pseudo RF options
+		JLabel pseudoRFLabel = new JLabel("Pseudo RF");
+		pseudoRFLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbcPseudoRFLabel = new GridBagConstraints();
+		gbcPseudoRFLabel.insets = new Insets(5, 5, 5, 5);
+		gbcPseudoRFLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbcPseudoRFLabel.gridx = 1;
+		gbcPseudoRFLabel.gridy = 0;
+		searchPanel.add(pseudoRFLabel, gbcPseudoRFLabel);
+		
+		pseudoRFComboBox = new JComboBox<String>();
+		pseudoRFComboBox.setModel(
+				new DefaultComboBoxModel<String>(
+						new String[] {"0", "1", "2", "3", "4", "5"}));
+		GridBagConstraints gbcPseudoRFComboBox = new GridBagConstraints();
+		gbcPseudoRFComboBox.insets = new Insets(5, 5, 5, 5);
+		gbcPseudoRFComboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbcPseudoRFComboBox.gridx = 2;
+		gbcPseudoRFComboBox.gridy = 0;
+		searchPanel.add(pseudoRFComboBox, gbcPseudoRFComboBox);
+		
+		// Add similarity options
+		JLabel similarityLabel = new JLabel("Similarity");
+		similarityLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbcSimilarityLabel = new GridBagConstraints();
+		gbcSimilarityLabel.insets = new Insets(5, 5, 5, 5);
+		gbcSimilarityLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbcSimilarityLabel.gridx = 3;
+		gbcSimilarityLabel.gridy = 0;
+		searchPanel.add(similarityLabel, gbcSimilarityLabel);
+		
+		similarityComboBox = new JComboBox<String>();
+		similarityComboBox.setModel(
+				new DefaultComboBoxModel<String>(
+						new String[] {"Cosine", "Jaccard", "Term Correlation"}));
+		GridBagConstraints gbcSimilarityComboBox = new GridBagConstraints();
+		gbcSimilarityComboBox.insets = new Insets(5, 5, 5, 5);
+		gbcSimilarityComboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbcSimilarityComboBox.gridx = 4;
+		gbcSimilarityComboBox.gridy = 0;
+		searchPanel.add(similarityComboBox, gbcSimilarityComboBox);
+		
+		// Add options for search type
+		searchTypeComboBox = new JComboBox<String>();
+		searchTypeComboBox.setModel(
+				new DefaultComboBoxModel<String>(
+						new String[] {"Search", "Refine Query"}));
+		GridBagConstraints gbcSearchTypeComboBox = new GridBagConstraints();
+		gbcSearchTypeComboBox.insets = new Insets(5, 5, 5, 5);
+		gbcSearchTypeComboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbcSearchTypeComboBox.gridx = 5;
+		gbcSearchTypeComboBox.gridy = 0;
+		searchPanel.add(searchTypeComboBox, gbcSearchTypeComboBox);
+	}
 
-	
 	private static void setUIFont(FontUIResource font)
 	{
 	    Enumeration<Object> keys = UIManager.getDefaults().keys();
@@ -89,141 +229,11 @@ public class Window {
 		catch (IllegalAccessException e) {
 			// handle exception
 		}
-	}
-
-	public static void setModel(ListModel<String> model) {
-		list.setModel(model);
+		
+		setUIFont(new FontUIResource("Segoe UI",Font.PLAIN,16));
 	}
 
 	public static String getQueryString() {
 		return textField.getText().trim();
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 * @wbp.parser.entryPoint
-	 */
-	public static void initialize() {
-		setUI();
-		setUIFont(new javax.swing.plaf.FontUIResource("Segoe UI",Font.PLAIN,16));
-
-		
-		frmLuceneSearchEngine = new JFrame();
-		frmLuceneSearchEngine.setTitle("Lucene Search Engine");
-		frmLuceneSearchEngine.getContentPane().setFont(new Font("Segoe UI Light", Font.PLAIN, 14));
-		frmLuceneSearchEngine.setSize(800, 500);
-		frmLuceneSearchEngine.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		
-		JPanel filePanel = new JPanel();
-		frmLuceneSearchEngine.getContentPane().add(filePanel, BorderLayout.SOUTH);
-		GridBagLayout gbl_filePanel = new GridBagLayout();
-		gbl_filePanel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_filePanel.rowHeights = new int[] {30};
-		gbl_filePanel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_filePanel.rowWeights = new double[]{0.0};
-		filePanel.setLayout(gbl_filePanel);
-		
-		btnNewButton_1 = new JButton("Import Query");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.insets = new Insets(5, 5, 5, 5);
-		gbc_btnNewButton_1.gridwidth = 6;
-		gbc_btnNewButton_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton_1.gridx = 0;
-		gbc_btnNewButton_1.gridy = 0;
-		filePanel.add(btnNewButton_1, gbc_btnNewButton_1);
-		
-		btnNewButton_2 = new JButton("Export Result");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.gridwidth = 6;
-		gbc_btnNewButton_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNewButton_2.insets = new Insets(5, 5, 5, 5);
-		gbc_btnNewButton_2.gridx = 6;
-		gbc_btnNewButton_2.gridy = 0;
-		filePanel.add(btnNewButton_2, gbc_btnNewButton_2);
-		
-		
-		JPanel searchPanel = new JPanel();
-		frmLuceneSearchEngine.getContentPane().add(searchPanel, BorderLayout.NORTH);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{127, 70, 50, 60, 100, 50, 0};
-		gbl_panel.rowHeights = new int[] {30};
-		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0};
-		searchPanel.setLayout(gbl_panel);
-
-		textField = new JTextField("Type query");
-		
-
-		textField.addActionListener(Controller.getInstance());
-		textField.setColumns(10);
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(5, 5, 5, 5);
-		gbc_textField.ipadx = 10;
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 0;
-		gbc_textField.gridy = 0;
-		searchPanel.add(textField, gbc_textField);
-		
-		lblNewLabel = new JLabel("Pseudo RF");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.insets = new Insets(5, 5, 5, 5);
-		gbc_lblNewLabel.fill = GridBagConstraints.HORIZONTAL;
-		gbc_lblNewLabel.gridx = 1;
-		gbc_lblNewLabel.gridy = 0;
-		searchPanel.add(lblNewLabel, gbc_lblNewLabel);
-		
-		comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5"}));
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.insets = new Insets(5, 5, 5, 5);
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.gridx = 2;
-		gbc_comboBox.gridy = 0;
-		searchPanel.add(comboBox, gbc_comboBox);
-		
-		lblNewLabel_1 = new JLabel("Similarity");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.insets = new Insets(5, 5, 5, 5);
-		gbc_lblNewLabel_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_lblNewLabel_1.gridx = 3;
-		gbc_lblNewLabel_1.gridy = 0;
-		searchPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
-		comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Cosine", "Jaccard", "Term Correlation"}));
-		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-		gbc_comboBox_1.insets = new Insets(5, 5, 5, 5);
-		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox_1.gridx = 4;
-		gbc_comboBox_1.gridy = 0;
-		searchPanel.add(comboBox_1, gbc_comboBox_1);
-		
-		comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Search", "Refine Query"}));
-		GridBagConstraints gbc_comboBox_2 = new GridBagConstraints();
-		gbc_comboBox_2.insets = new Insets(5, 5, 5, 5);
-		gbc_comboBox_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox_2.gridx = 5;
-		gbc_comboBox_2.gridy = 0;
-		searchPanel.add(comboBox_2, gbc_comboBox_2);
-
-		list = new JList<String>();
-		scrollPane = new JScrollPane(list);
-
-		frmLuceneSearchEngine.getContentPane().add(scrollPane, BorderLayout.CENTER);
-
-		frmLuceneSearchEngine.setVisible(true);
-	}
-
 }
