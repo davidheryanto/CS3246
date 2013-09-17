2961629611034c79bd8740e7a7375fd92d2fe563
William Lee <wksw9680@hotmail.com>
2013-09-17 21:55:08 +0800

    added query txt file

--- Project1/src/lucene/Constants.java
+++ Project1/src/lucene/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
 	public static final Version VERSION = Version.LUCENE_36;
 	public static final String DIR_PATH_DATA = "./data";
 	public static final String DIR_PATH_INDEX = "./index";
+	public static final String DIR_PATH_QUERY = "CS3246Project1_query.txt";
 	
 	
 	public static final String LABEL_BUTTON_SEARCH = "Search";
