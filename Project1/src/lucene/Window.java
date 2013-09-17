4b4df3afd44509584c639bfbd8c8659a8a0b90e7
David Heryanto <david.heryanto@hotmail.com>
2013-09-17 17:06:12 +0800

    Allow pressing Enter in text field to search

--- Project1/src/lucene/Window.java
+++ Project1/src/lucene/Window.java
@@ -8,19 +8,34 @@ import java.awt.Insets;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.ListModel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.JScrollPane;
 
 public class Window {
-	public static final String LABEL_BUTTON_SEARCH = "Search";
+	// Singleton pattern
+	private static final Window instance = new Window();
+
+	private Window() { }
+
+	public static Window getInstance() {
+		return instance;
+	}
 	
+
+
+
+
+
+	public static final String LABEL_BUTTON_SEARCH = "Search";
+
 	private static JFrame frame;
 	private static JTextField textField;
 	private static JScrollPane scrollPane;
@@ -46,10 +61,10 @@ public class Window {
 		}
 	}
 
-	public void setModel(ListModel<String> model) {
+	public static void setModel(ListModel<String> model) {
 		list.setModel(model);
 	}
-	
+
 	public static String getQueryString() {
 		return textField.getText().trim();
 	}
@@ -60,8 +75,8 @@ public class Window {
 	 */
 	public static void initialize() {
 		setUI();
-		
-		
+
+
 		frame = new JFrame();
 		frame.getContentPane().setFont(new Font("Segoe UI Light", Font.PLAIN, 14));
 		frame.setBounds(100, 100, 450, 300);
@@ -83,6 +98,8 @@ public class Window {
 				textField.selectAll();
 			}
 		});
+
+		textField.addActionListener(Controller.getInstance());
 		textField.setColumns(10);
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.gridwidth = 3;
@@ -93,90 +110,90 @@ public class Window {
 		panel.add(textField, gbc_textField);
 
 		JButton btnNewButton = new JButton(LABEL_BUTTON_SEARCH);
-		
-		
-		btnNewButton.addActionListener(Main.controller);
-		
-//		
-//		btnNewButton.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				
-//				
-//				
-//
-//				String queryString = textField.getText().trim();
-//				System.out.printf("------------------------%nSEARCHING...%n");
-//				SearchEngine instance = new SearchEngine();
-//
-//
-//				ScoreDoc[] hits = instance.performSearch(queryString, 10);
-//
-//				// remove all strings in model
-//				model.clear();
-//
-//				System.out.println("Results found: " + hits.length);
-//				for (int i = 0; i < hits.length; i++) {
-//					ScoreDoc hit = hits[i];
-//					// Document doc = hit.doc();
-//
-//					Query query = new QueryParser(VERSION, "title",
-//							new StandardAnalyzer(VERSION))
-//					.parse(queryString);
-//
-//					Explanation explanation = instance.searcher.explain(query, hit.doc);
-//
-//					Document doc = instance.searcher.doc(hits[i].doc); // This
-//
-//					String resultString = 	doc.get("id") + "|" + 
-//							doc.get("title") + "|" + 
-//							doc.get("author") + " (" + 
-//							hit.score + ")";
-//
-//					model.addElement(resultString);
-//
-//					System.out.println(resultString);
-//					System.out.println(explanation.toString());
-//
-//				}
-//				System.out.println("performSearch done");
-//
-//
-//
-//				TopDocs topDocs = search();
-//				print(topDocs); 
-//
-//
-//
-//
-//
-//				String queryString = textField.getText().trim();
-//
-//				SearchEngine instance = new SearchEngine();
-//				ScoreDoc[] hits = instance.performSearch(queryString, 10);
-//
-//
-//
-//
-//				System.out.printf("Search Query: %s%n", queryString);
-//			}
-//		});
-//		
-		
+
+
+		btnNewButton.addActionListener( Controller.getInstance() );
+
+		//		
+		//		btnNewButton.addActionListener(new ActionListener() {
+		//			public void actionPerformed(ActionEvent arg0) {
+		//				
+		//				
+		//				
+		//
+		//				String queryString = textField.getText().trim();
+		//				System.out.printf("------------------------%nSEARCHING...%n");
+		//				SearchEngine instance = new SearchEngine();
+		//
+		//
+		//				ScoreDoc[] hits = instance.performSearch(queryString, 10);
+		//
+		//				// remove all strings in model
+		//				model.clear();
+		//
+		//				System.out.println("Results found: " + hits.length);
+		//				for (int i = 0; i < hits.length; i++) {
+		//					ScoreDoc hit = hits[i];
+		//					// Document doc = hit.doc();
+		//
+		//					Query query = new QueryParser(VERSION, "title",
+		//							new StandardAnalyzer(VERSION))
+		//					.parse(queryString);
+		//
+		//					Explanation explanation = instance.searcher.explain(query, hit.doc);
+		//
+		//					Document doc = instance.searcher.doc(hits[i].doc); // This
+		//
+		//					String resultString = 	doc.get("id") + "|" + 
+		//							doc.get("title") + "|" + 
+		//							doc.get("author") + " (" + 
+		//							hit.score + ")";
+		//
+		//					model.addElement(resultString);
+		//
+		//					System.out.println(resultString);
+		//					System.out.println(explanation.toString());
+		//
+		//				}
+		//				System.out.println("performSearch done");
+		//
+		//
+		//
+		//				TopDocs topDocs = search();
+		//				print(topDocs); 
+		//
+		//
+		//
+		//
+		//
+		//				String queryString = textField.getText().trim();
+		//
+		//				SearchEngine instance = new SearchEngine();
+		//				ScoreDoc[] hits = instance.performSearch(queryString, 10);
+		//
+		//
+		//
+		//
+		//				System.out.printf("Search Query: %s%n", queryString);
+		//			}
+		//		});
+		//		
+
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.gridwidth = 6;
 		gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnNewButton.gridx = 3;
 		gbc_btnNewButton.gridy = 0;
 		panel.add(btnNewButton, gbc_btnNewButton);
-		
+
 		list = new JList<String>();
 		scrollPane = new JScrollPane(list);
 		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-		
-		
-		
-		
-		
+
+
+
+
+
 		frame.setVisible(true);
 	}
 
