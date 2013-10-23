a98730dd684166799d4c4d12bc4e80d51af1b149
William Lee <wksw9680@hotmail.com>
2013-10-24 02:16:18 +0800

    updated EdgeSimilarity

--- Project-2/Demo/src/imagesimilarity/EdgeSimilarity.java
+++ Project-2/Demo/src/imagesimilarity/EdgeSimilarity.java
@@ -3,16 +3,19 @@ package imagesimilarity;
 public class EdgeSimilarity {
 	
 	public static double getScore(ProcessedImage img1, ProcessedImage img2) {
-		//double score = 0.0;
+		double score = 0;
 		int[][] hist1 = img1.getEdgeHist();
 		int[][] hist2 = img2.getEdgeHist();
 		
-		double distance = calculateDistance(hist1, hist2);
+		for(int i = 0; i < 3; i++)
+			for(int j = 0; j < 256; j++)
+			{
+				double diff = Math.abs(hist1[i][j]-hist2[i][j]);
+				double max = Math.max(hist1[i][j], hist2[i][j]);
+				score += hist1[i][j] * (1 - (diff / max));
+			}
 		
-		return 1-distance;
+		return score;
 	}
 	
-	private double calculateDistance(int[][] array1, int[][] array2) {
-		
-	}
 }
