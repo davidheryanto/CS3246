b76b56edccce290725183696eee233ba093d3588
William Lee <wksw9680@hotmail.com>
2013-10-23 23:23:02 +0800

    Edited ColorSimilarity

--- Project-2/Demo/src/imagesimilarity/ColorSimilarity.java
+++ Project-2/Demo/src/imagesimilarity/ColorSimilarity.java
@@ -1,43 +1,44 @@
 package imagesimilarity;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.Raster;
-
 public class ColorSimilarity {
 	
-	public static double[][] computeColorSimilarity(BufferedImage image) {
-		int width = image.getWidth();
-		int height = image.getHeight();
-		double[][] colorSim = new double[width*height][width*height];
-		Raster raster = image.getRaster();
-		double[][] y = new double[width][height],
-				cb = new double[width][height],
-				cr = new double[width][height];
+	public static double[][] computeColorSimilarity() {
+		double Tcolor = 0.1;
+		int size = 256;
+		double[][] colorSim = new double[size*size*size][size*size*size];
+		double[][][] y = new double[size][size][size],
+				cb = new double[size][size][size],
+				cr = new double[size][size][size];
 		int n1=0, n2=0;
 		
-		for(int i = 0; i < width; i++)
-			for(int j = 0; j < height; j++)
-			{
-				int r = raster.getSample(i,j,0);
-            	int g = raster.getSample(i,j,1);
-            	int b = raster.getSample(i,j,2);
-            	y[i][j]  = (int)( 0.299   * r + 0.587   * g + 0.114   * b);
-        		cb[i][j] = (int)(-0.16874 * r - 0.33126 * g + 0.50000 * b);
-        		cr[i][j] = (int)( 0.50000 * r - 0.41869 * g - 0.08131 * b);
-			}
+		for(int i = 0; i < size; i++)
+			for(int j = 0; j < size; j++)
+				for(int k = 0; k < size; k++)
+				{
+					int r = i;
+	            	int g = j;
+	            	int b = k;
+	            	y[i][j][k]  = (int)( 0.299   * r + 0.587   * g + 0.114   * b);
+	        		cb[i][j][k] = (int)(-0.16874 * r - 0.33126 * g + 0.50000 * b);
+	        		cr[i][j][k] = (int)( 0.50000 * r - 0.41869 * g - 0.08131 * b);
+				}
 		
-		for(int i = 0; i < width; i++)
-			for(int j = 0; j < height; j++)
-			{
-				for(int ii = 0; ii < width; ii++)
-					for(int jj = 0; jj < height; jj++)
-					{
-						double dist = Math.sqrt( Math.pow(y[i][j]-y[ii][jj], 2) + Math.pow(cb[i][j]-cb[ii][jj], 2) + Math.pow(cr[i][j]-cr[ii][jj], 2) );
-						colorSim[n1][n2] = dist;
-						n2++;
-					}
-				n1++;
-			}
+		for(int i = 0; i < size; i++)
+			for(int j = 0; j < size; j++)
+				for(int k = 0; k < size; k++)
+				{
+					for(int ii = 0; ii < size; ii++)
+						for(int jj = 0; jj < size; jj++)
+							for(int kk = 0; kk < size; kk++)
+						{
+							double dist = Math.sqrt( Math.pow(y[i][j][k]-y[ii][jj][kk], 2) + 
+										Math.pow(cb[i][j][k]-cb[ii][jj][kk], 2) + 
+										Math.pow(cr[i][j][k]-cr[ii][jj][kk], 2) );
+							colorSim[n1][n2] = 1 - (dist/Tcolor);
+							n2++;
+						}
+					n1++;
+				}
 		
 		return colorSim;
 	}
