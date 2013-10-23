a22456f42df7ad62739762505382186aec90ca87
David Heryanto <david.heryanto@hotmail.com>
2013-10-24 00:53:45 +0800

    Update isChecked... vars

--- Project-2/Demo/src/imagesimilarity/Searcher.java
+++ Project-2/Demo/src/imagesimilarity/Searcher.java
@@ -2,8 +2,19 @@ package imagesimilarity;
 
 public class Searcher {
 	
-	public int isCheckedCCV;
-	public int isCheckedEdge;
+	private boolean isCheckedNormalHistogram;
+	private boolean isCheckedCCV;
+	private boolean isCheckedEdge;
+	
+	public void setCheckedNormalHistogram(boolean isCheckedNormalHistogram) {
+		this.isCheckedNormalHistogram = isCheckedNormalHistogram;
+	}
+	public void setCheckedCCV(boolean isCheckedCCV) {
+		this.isCheckedCCV = isCheckedCCV;
+	}
+	public void setCheckedEdge(boolean isCheckedEdge) {
+		this.isCheckedEdge = isCheckedEdge;
+	}
 	
 	
 }
