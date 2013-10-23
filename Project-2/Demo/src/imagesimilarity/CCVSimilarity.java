c867dcc4161caca114a29fe201674651079426ce
David Heryanto <david.heryanto@hotmail.com>
2013-10-23 23:52:24 +0800

    Add IndexedImage Class

---
+++ Project-2/Demo/src/imagesimilarity/CCVSimilarity.java
@@ -0,0 +1,11 @@
+package imagesimilarity;
+
+import java.awt.image.BufferedImage;
+
+public class CCVSimilarity {
+	public static double getScore(BufferedImage img1, BufferedImage img2) {
+		
+		
+		return 0;
+	}
+}
---
+++ Project-2/Demo/src/imagesimilarity/IndexedImage.java
@@ -0,0 +1,8 @@
+package imagesimilarity;
+
+import imagesimilarity.ColorCoherence.Result;
+
+public class IndexedImage {
+	private String filePath;
+	private Result[] CCV;
+}
