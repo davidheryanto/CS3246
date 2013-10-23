538ff47a0aebdb670106f4b1bc15fcf507629ac6
David Heryanto <david.heryanto@hotmail.com>
2013-10-24 02:49:43 +0800

    Merge

--- Project-2/Demo/src/imagesimilarity/Searcher.java
+++ Project-2/Demo/src/imagesimilarity/Searcher.java
@@ -14,12 +14,14 @@ public class Searcher {
 	public void setCheckedNormalHistogram(boolean isCheckedNormalHistogram) {
 		this.isCheckedNormalHistogram = isCheckedNormalHistogram;
 	}
+	
 	public void setCheckedCCV(boolean isCheckedCCV) {
 		this.isCheckedCCV = isCheckedCCV;
 	}
+	
 	public void setCheckedEdge(boolean isCheckedEdge) {
 		this.isCheckedEdge = isCheckedEdge;
-	
+	}
 	
 	public String[] search(BufferedImage inputImage) throws IOException {
 		String[] rankedResults;// = new String[20];
