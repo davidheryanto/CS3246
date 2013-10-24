d8fa047c38c39b7d335e73f96f39e07d4df5a683
William Lee <wksw9680@hotmail.com>
2013-10-24 17:27:25 +0800

    Updated searcher to process input image

--- Project-2/Demo/src/imagesimilarity/Searcher.java
+++ Project-2/Demo/src/imagesimilarity/Searcher.java
@@ -2,6 +2,7 @@ package imagesimilarity;
 
 import imagesimilarity.ColorCoherence.Result;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Arrays;
@@ -32,7 +33,7 @@ public class Searcher {
 		double[] scores;
 		ProcessedImage img = new ProcessedImage();
 		
-		//TODO: Process input image
+		img = processImage(inputImage);
 		
 //		scores = computeSimilarity(img);	
 //		
@@ -47,6 +48,56 @@ public class Searcher {
 //		return rankedResults;
 	}
 	
+	private ProcessedImage processImage(BufferedImage image) {
+		ProcessedImage img = new ProcessedImage();
+		
+		if(isCheckedNormalHistogram)
+		{
+			int[][] hist = getHistogram(image);
+			img.setColorHist(hist);
+		}
+		
+		if(isCheckedEdge)
+		{
+			BufferedImage result = FilterSobel.apply(image);
+			int[][] hist = getHistogram(result);
+			img.setEdgeHist(hist);
+		}
+		
+		if(isCheckedCCV)
+		{
+			image = ImageHelper.resize(image, 100);
+			ColorCoherence.extract(image);
+			Result[] results = ColorCoherence.getResults();
+			img.setCCV(results);
+		}
+		
+		return img;
+	}
+	
+	private int[][] getHistogram(BufferedImage image) {
+		int width = image.getWidth();
+		int height = image.getHeight();
+		int[][] hist = new int[3][256];
+				
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 256; j++) {
+				hist[i][j] = 0;
+			}
+		}
+		
+		for(int i = 0; i < width; i++)
+			for(int j = 0; j < height; j++) 
+			{
+				Color c = new Color(image.getRGB(i, j));
+				hist[0][c.getRed()]++;
+				hist[1][c.getGreen()]++;
+				hist[2][c.getBlue()]++;
+			}
+		
+		return hist;
+	}
+	
 	private double[] computeSimilarity(ProcessedImage img1) throws IOException {
 		double[] scores = new double[400];
 		
@@ -57,20 +108,20 @@ public class Searcher {
 			if(isCheckedNormalHistogram & !isCheckedCCV & !isCheckedEdge)
 			{
 				int[][] hist;
-				hist = Indexer.readIndex(Integer.toString(i), NormalIndex);
+				hist = Indexer.readIndex(Integer.toString(i+1), NormalIndex);
 				img2.setColorHist(hist);
 				scores[i] = NormalSimilarity.getScore(img1, img2);
 			}
 			if(isCheckedCCV & !isCheckedEdge)
 			{
-				Result[] results = Indexer.readIndexCCV(Integer.toString(i), CCVIndex);
+				Result[] results = Indexer.readIndexCCV(Integer.toString(i+1), CCVIndex);
 				img2.setCCV(results);
 				scores[i] = CCVSimilarity.getScore(img1, img2);
 			}
 			if(!isCheckedCCV & isCheckedEdge)
 			{
 				int[][] hist;
-				hist = Indexer.readIndex(Integer.toString(i),EdgeIndex);
+				hist = Indexer.readIndex(Integer.toString(i+1),EdgeIndex);
 				img2.setEdgeHist(hist);
 				scores[i] = EdgeSimilarity.getScore(img1, img2);
 			}
