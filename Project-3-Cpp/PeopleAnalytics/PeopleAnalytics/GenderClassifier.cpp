69f7decd53a6c42332e5b5d2799e4cd64c3eef3a
David Heryanto <david.heryanto@hotmail.com>
2013-11-16 03:08:44 +0800

    Add SmileDetector

--- Project-3-Cpp/PeopleAnalytics/PeopleAnalytics/GenderClassifier.cpp
+++ Project-3-Cpp/PeopleAnalytics/PeopleAnalytics/GenderClassifier.cpp
@@ -75,7 +75,7 @@ static void read_csv(const string& filename, vector<Mat>& images, vector<int>& l
 	}
 }
 
-int main(int argc, const char *argv[]) {
+int main2(int argc, const char *argv[]) {
 	// Check for valid command line arguments, print usage
 	// if no arguments were given.
 	if (argc < 2) {
