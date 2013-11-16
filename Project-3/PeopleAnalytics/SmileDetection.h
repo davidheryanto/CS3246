531d3513604db78ed5f0cd6dd93e7f6dce9392bb
David Heryanto <david.heryanto@hotmail.com>
2013-11-16 22:22:48 +0800

    Update SmileDetection.h

--- Project-3/PeopleAnalytics/SmileDetection.h
+++ Project-3/PeopleAnalytics/SmileDetection.h
@@ -1,13 +1,27 @@
 #pragma once
 
 #include "opencv2\core\core.hpp"
+#include "opencv2\contrib\contrib.hpp"
+#include "opencv2\highgui\highgui.hpp"
 
-// face is assumed to have size 150px X 150px
-// aligned properly.
-// Returns true if the face is smiling
-// by utilizing FisherFaceRecognizer method.
-bool isSmiling(cv::Mat face);
+class SmileDetection
+{
+public:
+	SmileDetection();
 
+	~SmileDetection();
 
+	// face is assumed to have size 150px X 150px
+	// aligned properly.
+	// Returns true if the face is smiling
+	// by utilizing FisherFaceRecognizer method.
+	bool isSmiling(cv::Mat face);
 
+private:
+	std::vector<cv::Mat> images;
+	std::vector<int> labels;
+	cv::Ptr<cv::FaceRecognizer> model;
 
+	void ReadCsv(std::string csvPath);
+	cv::Mat Normalize(cv::InputArray src);
+};
