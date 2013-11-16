0192c4ee38bd09cc1a90d7c7d416542423e63239
William Lee <wksw9680@hotmail.com>
2013-11-17 01:44:53 +0800

    face detection with webcam added, no output yet

--- Project-3/PeopleAnalytics/Main.cpp
+++ Project-3/PeopleAnalytics/Main.cpp
@@ -32,16 +32,54 @@ passing each face to GenderDetection and SmileDetection to get the data for each
 
 */
 
+/** Function Headers */
 void RunGenderDetection();
+void detectAndDisplay( Mat frame );
+
+/** Global variables */
+String face_cascade_name = "data\\haarcascades\\haarcascade_frontalface_alt.xml";
+CascadeClassifier face_cascade;
+string window_name = "Capture - Face detection";
 
 int main(int argc, const char** argv)
 {
-	RunGenderDetection();
+	CvCapture* capture;
+	Mat frame;
+ 
+	//-- 1. Load the cascades
+	if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+
+	//-- 2. Read the video stream
+	capture = cvCaptureFromCAM( CV_CAP_ANY );
+	if( capture )
+	{
+		frame = cvQueryFrame( capture );
+		while( true )
+		{
+			frame = cvQueryFrame( capture );
+			//imshow( window_name, frame );
+			//-- 3. Apply the classifier to the frame
+			if( !frame.empty() )
+				detectAndDisplay( frame );
+			else
+			{
+				printf(" --(!) No captured frame -- Break!");
+				break;
+			}
+ 
+			int c = waitKey(10);
+			if( (char)c == 'c' )
+				break;
+		}
+	}
+
+	/*RunGenderDetection();
 	cout << "Gender detection finished" << endl;
 
 
-	getchar();
-
+	getchar();*/
+	cvReleaseCapture(&capture); //Release capture.
+    destroyWindow(window_name); //Destroy Window
 	return 0;
 }
 
@@ -85,4 +123,24 @@ void RunGenderDetection()
 		}
 		cout << endl;
 	}
+}
+
+void detectAndDisplay( Mat frame )
+{
+	std::vector<Rect> faces;
+	Mat frame_gray;
+ 
+	cvtColor( frame, frame_gray, CV_BGR2GRAY );
+	equalizeHist( frame_gray, frame_gray );
+ 
+	//-- Detect faces
+	face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+	for( int i = 0; i < faces.size(); i++ )
+	{
+		Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+		//ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+		rectangle(frame, faces[i], Scalar( 255, 0, 255 ), 2, 8, 0);
+	}
+	//-- Show what you got
+	imshow( window_name, frame );
 }
