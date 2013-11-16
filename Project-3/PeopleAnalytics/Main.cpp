86f0e014e85c46ffc59063bc43c37afec40b77b6
David Heryanto <david.heryanto@hotmail.com>
2013-11-16 12:57:13 +0800

    Update Project-3

--- Project-3/PeopleAnalytics/Main.cpp
+++ Project-3/PeopleAnalytics/Main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
-
+#include <cv.h>
+#include <highgui.h>
+ 
 using namespace std;
-
-int main(int argc, char* argv[])
+char key;
+int main()
 {
-	cout << "Hello";
-	getchar();
-
-	return 0;
+    cvNamedWindow("Camera_Output", 1);    //Create window
+    CvCapture* capture = cvCaptureFromCAM(CV_CAP_ANY);  //Capture using any camera connected to your system
+    while(1){ //Create infinte loop for live streaming
+ 
+        IplImage* frame = cvQueryFrame(capture); //Create image frames from capture
+        cvShowImage("Camera_Output", frame);   //Show image frames on created window
+        key = cvWaitKey(10);     //Capture Keyboard stroke
+        if (char(key) == 27){
+            break;      //If you hit ESC key loop will break.
+        }
+    }
+    cvReleaseCapture(&capture); //Release capture.
+    cvDestroyWindow("Camera_Output"); //Destroy Window
+    return 0;
 }
